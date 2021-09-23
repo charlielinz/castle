db5b266006f52c54e80dd34107dae91389d2a25a
charlielinz <ilovealinlin@gmail.com>
2021-09-23 19:41:57 +0800

    Update MobileDropdownMenu.js

--- components/MobileDropdownMenu.js
+++ components/MobileDropdownMenu.js
@@ -10,52 +10,52 @@ const MobileDropdownMenu = ({ isOpened, setIsOpened }) => {
             : "invisible w-40 p-4 bg-white rounded-lg shadow-md transition duration-500 opacity-0"
         }
       >
-        <li className="p-2 text-xl rounded-sm">
-          <span onClick={() => setIsOpened(false)}>
-            <Link href="/code">
+        <li className="flex p-2 text-xl rounded-sm">
+          <span className="w-full" onClick={() => setIsOpened(false)}>
+            <Link href="/code" className="w-full">
               <a>Code Hub</a>
             </Link>
           </span>
         </li>
-        <li className="p-2 text-xl rounded-sm">
-          <span onClick={() => setIsOpened(false)}>
+        <li className="flex p-2 text-xl rounded-sm">
+          <span className="w-full" onClick={() => setIsOpened(false)}>
             <Link href="/record">
               <a>Music Hall</a>
             </Link>
           </span>
-          <ul className="px-4 py-2">
-            <li className="px-2 py-2 text-base rounded-sm">
-              <span onClick={() => setIsOpened(false)}>
-                <Link href="/record#piano-solo-recital">
-                  <a>
-                    <p>2019.06.18</p>
-                    <p>Piano Recital</p>
-                  </a>
-                </Link>
-              </span>
-            </li>
-            <li className="px-2 py-2 text-base rounded-sm">
-              <span onClick={() => setIsOpened(false)}>
-                <Link href="/record#chamber-music-recital-1">
-                  <a>
-                    <p>2018.06.23</p>
-                    <p>Chamber Music Recital</p>
-                  </a>
-                </Link>
-              </span>
-            </li>
-            <li className="px-2 py-2 text-base rounded-sm">
-              <span onClick={() => setIsOpened(false)}>
-                <Link href="/record#chamber-music-recital-2">
-                  <a>
-                    <p>2018.06.03</p>
-                    <p>Chamber Music Recital</p>
-                  </a>
-                </Link>
-              </span>
-            </li>
-          </ul>
         </li>
+        <ul className="px-4 py-2">
+          <li className="px-2 py-2 text-base rounded-sm">
+            <span onClick={() => setIsOpened(false)}>
+              <Link href="/record#piano-solo-recital">
+                <a>
+                  <p>2019.06.18</p>
+                  <p>Piano Recital</p>
+                </a>
+              </Link>
+            </span>
+          </li>
+          <li className="px-2 py-2 text-base rounded-sm">
+            <span onClick={() => setIsOpened(false)}>
+              <Link href="/record#chamber-music-recital-1">
+                <a>
+                  <p>2018.06.23</p>
+                  <p>Chamber Music Recital</p>
+                </a>
+              </Link>
+            </span>
+          </li>
+          <li className="px-2 py-2 text-base rounded-sm">
+            <span onClick={() => setIsOpened(false)}>
+              <Link href="/record#chamber-music-recital-2">
+                <a>
+                  <p>2018.06.03</p>
+                  <p>Chamber Music Recital</p>
+                </a>
+              </Link>
+            </span>
+          </li>
+        </ul>
       </ul>
     </div>
   );
