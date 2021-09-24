da6b9d3b0311610fba82d58b497c3ee111a85601
charlielinz <ilovealinlin@gmail.com>
2021-09-24 23:07:22 +0800

    Update CodeProjectHolder.js

--- components/CodeProjectHolder.js
+++ components/CodeProjectHolder.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import imiterReport from "../public/img/imiter-report.png";
 
 const CodeProjectHolder = () => {
-  const windowWidth = useWindowWidth();
   return (
     <header className="bg-gray-50 font-san text-gray-900">
       <div className="max-w-sm mx-auto px-8 pt-20 pb-12 flex justify-center lg:max-w-screen-lg lg:px-4 lg:py-28">
