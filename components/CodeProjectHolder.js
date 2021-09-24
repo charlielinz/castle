a082e5e9a037144de4294c69d29e394057fb9f14
charlielinz <ilovealinlin@gmail.com>
2021-09-24 23:01:49 +0800

    Update Code Hub syntax

--- components/CodeProjectHolder.js
+++ components/CodeProjectHolder.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import imiterReport from "../public/img/imiter-report.png";
-import useWindowWidth from "../hooks/useWindowWidth";
 
 const CodeProjectHolder = () => {
   const windowWidth = useWindowWidth();
@@ -12,59 +11,27 @@ const CodeProjectHolder = () => {
         </p>
       </div>
       <div className="max-w-sm mx-auto px-8 pt-12 pb-12 lg:max-w-screen-lg lg:px-4 lg:py-28">
-        {windowWidth > 1024 ? (
-          <>
-            <div className="flex items-center mx-6 pb-4">
-              <p className="fas fa-robot text-8xl pr-4"></p>
-              <div className="px-3">
-                <h1 className="text-4xl font-extrabold pb-2">
-                  Imiter
-                </h1>
-                <p className="text-lg text-gray-600">
-                  To test your apps with emulator automaticlly.
-                </p>
-              </div>
-            </div>
-            <div className="relative flex mx-6">
-              <video autoPlay loop muted playsInline>
-                <source src="/video/imiter.mp4" type="video/mp4"></source>
-              </video>
-              <div className="absolute w-3/5 top-6 right-4">
-                <Image
-                  src={imiterReport}
-                  alt="imiter report"
-                  className="rounded-sm"
-                />
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="lg:mx-6">
-              <div className="flex">
-                <p className="fas fa-robot mt-1 pt-0.5 pr-2 text-xl"></p>
-                <h1 className="text-2xl">
-                  Imiter
-                </h1>
-              </div>
-              <p className="py-2 px-1 text-gray-600 font-light">
-                To test your apps with emulator automaticlly.
-              </p>
-            </div>
-            <div className="relative flex">
-              <video autoPlay loop muted playsInline>
-                <source src="/video/imiter.mp4" type="video/mp4"></source>
-              </video>
-              <div className="absolute w-3/5 top-6 right-4">
-                <Image
-                  src={imiterReport}
-                  alt="imiter report"
-                  className="rounded-sm"
-                />
-              </div>
-            </div>
-          </>
-        )}
+        <div className="grid grid-flow-row grid-cols-13 grid-rows-2 lg:mx-6 lg:pb-4">
+          <span className="fas fa-robot text-2xl mt-3 ml-1 pt-0.5 lg:text-8xl lg:row-span-2 lg:col-span-2"></span>
+          <h1 className="text-2xl col-start-2 col-end-13 mt-3 ml-5 lg:text-4xl lg:font-extrabold lg:col-start-3 lg:col-end-13 lg:mt-4 lg:ml-1">
+            Imiter
+          </h1>
+          <p className="text-sm text-gray-600 col-start-1 col-end-13 ml-1 mb-2 lg:text-lg lg:col-start-3 lg:col-end-13 lg:mt-3">
+            To test your apps with emulator automaticlly.
+          </p>
+        </div>
+        <div className="relative flex lg:mx-6">
+          <video autoPlay loop muted playsInline>
+            <source src="/video/imiter.mp4" type="video/mp4"></source>
+          </video>
+          <div className="absolute w-3/5 top-6 right-4">
+            <Image
+              src={imiterReport}
+              alt="imiter report"
+              className="rounded-sm"
+            />
+          </div>
+        </div>
       </div>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
       },
       spacing: {
         "66": "16.5rem"
+      },
+      gridTemplateRows: {
+        "12": "repeat(12, minmax(0, 1fr))"
+      },
+      gridTemplateColumns: {
+        "13": "repeat(13, minmax(0, 1fr))"
+      },
+      gridRow: {
+        "span-7": "span 7 / span 7",
+        "span-8": "span 8 / span 8",
+        "span-9": "span 9 / span 9",
+        "span-10": "span 10 / span 10",
+        "span-11": "span 11 / span 11",
+        "span-12": "span 12 / span 12"
       }
     },
     fontFamily: {
