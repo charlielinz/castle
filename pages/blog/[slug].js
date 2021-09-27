4c07dfcdb4d4497f1b22efa398a6996a8ec0f1c0
charlielinz <ilovealinlin@gmail.com>
2021-09-28 00:36:08 +0800

    Update Blog
    -write some post
    -figure out img issue

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,7 +2,7 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import { getFrontmatter, getContent, getPostPaths } from "../../posts/info";
 import Image from "next/image";
-import marked from "marked";
+import ReactMarkdown from "react-markdown";
 
 const Postpage = ({
   slug,
@@ -14,13 +14,13 @@ const Postpage = ({
       <Navbar />
       <header className="bg-gray-50 font-san text-gray-900">
         <div className="max-w-sm mx-auto px-4 pt-8 pb-8 flex justify-center lg:max-w-screen-lg lg:px-4 lg:py-20">
-          <div className="w-11/12 bg-white rounded-md shadow-md lg:grid lg:grid-cols-3 lg:w-3/4 lg:shadow-xl lg:p-8">
+          <div className="w-11/12 bg-white rounded-md shadow-md lg:grid lg:grid-cols-3 lg:shadow-xl lg:p-8">
             <div>
               <p className="text-2xl px-4 pt-4 lg:text-3xl lg:p-4">{title}</p>
               <p className="text-sm px-4 py-2 lg:text-base lg:px-4">{date}</p>
               <p className="text-sm px-4 lg:text-base lg:p-4">{tag}</p>
             </div>
-            <div className="relative mx-auto my-4 h-60 w-11/12 lg:col-start-2 lg:my-0 lg:ml-10 lg:h-80 lg:w-96">
+            <div className="relative mx-auto my-4 h-60 w-11/12 lg:col-start-2 lg:col-end-4 lg:justify-self-stretch lg:my-0 lg:h-80 lg:w-96">
               <Image
                 src={cover_image}
                 alt="cover image"
@@ -28,10 +28,21 @@ const Postpage = ({
                 objectFit="contain"
               />
             </div>
-            <p
-              className="p-4 lg:row-start-2 lg:p-4"
-              dangerouslySetInnerHTML={{ __html: marked(content) }}
-            ></p>
+            <ReactMarkdown
+              className="prose p-4 lg:min-w-full lg:col-start-1 lg:col-end-4 "
+              components={{
+                img({ src, title }) {
+                  return (
+                    <>
+                      <span className="block">{title}</span>
+                      <img src={src} className="inline w-auto lg:h-64" />
+                    </>
+                  );
+                },
+              }}
+            >
+              {content}
+            </ReactMarkdown>
           </div>
         </div>
       </header>
