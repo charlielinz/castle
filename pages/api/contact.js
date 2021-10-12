b9fccfee9b41a5ba27c56ae86804cf56d6155f8c
charlielinz <ilovealinlin@gmail.com>
2021-10-13 00:41:06 +0800

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -10,6 +10,19 @@ const transporter = nodemailer.createTransport({
   secure: true,
 });
 
+await new Promise((resolve, reject) => {
+  // verify connection configuration
+  transporter.verify(function (error, success) {
+      if (error) {
+          console.log(error);
+          reject(error);
+      } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+      }
+  });
+});
+
 const sendEmail = async (body) => {
   const mailData = {
     to: "charlielin.org@gmail.com",
