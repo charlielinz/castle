66292458a917a8c2bd4e2d131f659d043a4f2342
charlielinz <ilovealinlin@gmail.com>
2021-10-13 01:02:30 +0800

    Update: use promise

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
   secure: true,
 });
 
-const sendEmail = async (body) => {
+const sendEmail = async (body, resolve, reject) => {
   const mailData = {
     to: "charlielin.org@gmail.com",
     subject: `Message From ${body.name}`,
@@ -21,14 +21,18 @@ const sendEmail = async (body) => {
   transporter.sendMail(mailData, function (err, info) {
     if (err) {
       console.error(err);
+      reject(err);
     } else {
       console.log(info);
+      resolve(info);
     }
   });
 };
 
 const handler = async (req, res) => {
-  await sendEmail(req.body);
+  await new Promise((resolve, reject) => {
+    sendEmail(req.body,resolve, reject);
+  });
   return res.status(200).json({ message: `Email sent successfuly` });
 };
 
