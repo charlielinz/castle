34aaa2f8c54114e02073cee589f5f83e5b000592
charlielinz <ilovealinlin@gmail.com>
2021-12-06 18:31:25 +0800

    use react hook form

---
+++ components/MessageForm.js
@@ -0,0 +1,96 @@
+import { useForm } from "react-hook-form";
+
+const MessageForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = (data) => {
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(() => {
+      isSubmitSuccessful ? reset({ name: "", email: "", message: "" }) : null;
+    });
+  };
+
+  return (
+    <header className="bg-gray-50 font-san text-gray-900">
+      <div className="max-w-sm mx-auto px-8 pt-28 pb-8 lg:max-w-screen-lg lg:px-4 lg:py-28">
+        <p className="flex justify-center text-3xl lg:font-black lg:text-4xl">
+          Contact me.
+        </p>
+      </div>
+      <div className="max-w-sm mx-auto px-8 pb-8 lg:max-w-screen-lg lg:px-4">
+        <form
+          className="text-lg flex flex-col items-center gap-4 pt-8 lg:text-xl lg:pt-0"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col w-4/5 lg:w-1/2">
+            <label htmlFor="name">Name</label>
+            <input
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "Please write down your name.",
+                },
+              })}
+              placeholder="Charlie Lin"
+              className="mt-2 mb-2 px-2 py-2 rounded-sm focus:outline-none"
+            />
+            <p className="mb-2 text-sm text-red-500">
+              {errors.name && errors.name.message}
+            </p>
+            <label htmlFor="email">Email</label>
+            <input
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Please write down your email.",
+                },
+                pattern: {
+                  value:
+                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: "Please write down a valid email.",
+                },
+              })}
+              placeholder="example@gmail.com"
+              className="mt-2 mb-2 px-2 py-2 rounded-sm focus:outline-none"
+            />
+            <p className="mb-2 text-sm text-red-500">
+              {errors.email && errors.email.message}
+              {errors.email && errors.email.pattern}
+            </p>
+            <label htmlFor="message">Message</label>
+            <textarea
+              {...register("message", {
+                required: {
+                  value: true,
+                  message: "Please write down the message.",
+                },
+              })}
+              className="mt-2 mb-2 px-2 py-2 h-32 rounded-sm focus:outline-none"
+            />
+            <p className="mb-2 text-sm text-red-500">
+              {errors.message && errors.message.message}
+            </p>
+            <input
+              type="submit"
+              value="Send it!"
+              className="appearance-none bg-truegray-300 rounded-lg py-2 mt-2 hover:bg-truegray-200 transition duration-300 focus:outline-none cursor-pointer"
+            />
+          </div>
+        </form>
+      </div>
+    </header>
+  );
+};
+
+export default MessageForm;
--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar";
 import ContactForm from "../components/ContactForm";
+import MessageForm from "../components/MessageForm";
 import Footer from "../components/Footer";
 
 const contact = () => {
   return (
     <>
       <Navbar />
-      <ContactForm />
+      <MessageForm />
       <Footer />
     </>
   );
